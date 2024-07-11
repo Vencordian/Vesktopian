8816ce5e2e5adf47ec9498ce108e15ce4c3b111d
RobinRMC <github@robinmail.nl>
2024-07-11 10:07:49 +0200

    Update Vesktop+

--- src/main/mainWindow.ts
+++ src/main/mainWindow.ts
@@ -91,7 +91,7 @@ function initTray(win: BrowserWindow) {
             click: createAboutWindow
         },
         {
-            label: "Repair Vencord",
+            label: "Repair Vencord+",
             async click() {
                 await downloadVencordFiles();
                 app.relaunch();
@@ -99,7 +99,7 @@ function initTray(win: BrowserWindow) {
             }
         },
         {
-            label: "Reset Vesktop",
+            label: "Reset Vesktop+",
             async click() {
                 await clearData(win);
             }
@@ -124,15 +124,15 @@ function initTray(win: BrowserWindow) {
     ]);
 
     tray = new Tray(ICON_PATH);
-    tray.setToolTip("Vesktop");
+    tray.setToolTip("Vesktop+");
     tray.setContextMenu(trayMenu);
     tray.on("click", onTrayClick);
 }
 
 async function clearData(win: BrowserWindow) {
     const { response } = await dialog.showMessageBox(win, {
-        message: "Are you sure you want to reset Vesktop?",
-        detail: "This will log you out, clear caches and reset all your settings!\n\nVesktop will automatically restart after this operation.",
+        message: "Are you sure you want to reset Vesktop+?",
+        detail: "This will log you out, clear caches and reset all your settings!\n\nVesktop+ will automatically restart after this operation.",
         buttons: ["Yes", "No"],
         cancelId: MessageBoxChoice.Cancel,
         defaultId: MessageBoxChoice.Default,
@@ -161,24 +161,24 @@ function initMenuBar(win: BrowserWindow) {
 
     const subMenu = [
         {
-            label: "About Vesktop",
+            label: "About Vesktop+",
             click: createAboutWindow
         },
         {
-            label: "Force Update Vencord",
+            label: "Force Update Vencord+",
             async click() {
                 await downloadVencordFiles();
                 app.relaunch();
                 app.quit();
             },
-            toolTip: "Vesktop will automatically restart after this operation"
+            toolTip: "Vesktop+ will automatically restart after this operation"
         },
         {
             label: "Reset Vesktop",
             async click() {
                 await clearData(win);
             },
-            toolTip: "Vesktop will automatically restart after this operation"
+            toolTip: "Vesktop+ will automatically restart after this operation"
         },
         {
             label: "Relaunch",
@@ -247,7 +247,7 @@ function initMenuBar(win: BrowserWindow) {
 
     const menu = Menu.buildFromTemplate([
         {
-            label: "Vesktop",
+            label: "Vesktop+",
             role: "appMenu",
             submenu: subMenu.filter(isTruthy)
         },
@@ -424,7 +424,7 @@ function createMainWindow() {
             transparencyOption !== "none" && {
                 transparent: true
             }),
-        ...(staticTitle && { title: "Vesktop" }),
+        ...(staticTitle && { title: "Vesktop+" }),
         ...(process.platform === "darwin" && getDarwinOptions()),
         ...getWindowBoundsOptions(),
         autoHideMenuBar: enableMenu
